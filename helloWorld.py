45a70aa98f9fc41dbba8e0f3692a292ec1350423
Michael Campbell <michaelc.5511@gmail.com>
2024-02-12 12:49:11 +0000

    descriptive username variable added and commented.

--- helloWorld.py
+++ helloWorld.py
@@ -1,13 +1,15 @@
 print("Git is awesome")
-info_out = input("What is your name? ")
-print(info_out)
+
+# Code to take users first name.
+user_name = input("What is your first name? ")
+print(user_name)
 
 # Code to take user input. Numeric vales only otherwise question is repeated.
 i=1
 while i==1:
     user_age = input("What is your age in years? ")
     if user_age.isnumeric():
-        print(f"{info_out} is: {user_age} years old.")
+        print(f"{user_name} is: {user_age} years old.")
         i=0
     else:
         print("You have not entered a valid number. Please try again.")
